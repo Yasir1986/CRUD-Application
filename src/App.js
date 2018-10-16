3b3e6c6acecbd046e465b1d5041d2cacf9bf00d4
Saidur Rahman <swesaon@gmail.com>
2018-10-16 17:23:44 +0600

    minor styling

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     return (
     <Provider store={store}>
       <div className="App">
-               <UserList />
+          <UserList />
       </div>
     </Provider>
     );
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,76 +1,96 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { getUsers, getDelete, addUsers, getEdit, updateUser } from '../actions/userAction';
-import PropTypes from 'prop-types';
-import AddUser from './AddUser';
-import EditUser from './EditUser';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import {
+  getUsers,
+  getDelete,
+  addUsers,
+  getEdit,
+  updateUser
+} from "../actions/userAction";
+import PropTypes from "prop-types";
+import AddUser from "./AddUser";
+import EditUser from "./EditUser";
 
 class UserList extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-        isEditing: false,
-        editUserData:{},
-      };
+      isEditing: false,
+      editUserData: {}
+    };
   }
-  componentDidMount () {
+  componentDidMount() {
     this.props.getUsers();
   }
-  handleDelete = (name) => {
+  handleDelete = name => {
     this.props.getDelete(name);
     console.log(this.props.getDelete(name));
-  }
-  handleSubmit = (values) => {
+  };
+  handleSubmit = values => {
     this.props.addUsers(values);
-  }
-  handleEdit = (user) => {
-    this.setState ({
+  };
+  handleEdit = user => {
+    this.setState({
       isEditing: true,
-      editUserData:user,
+      editUserData: user
     });
     this.props.getEdit(user);
     console.log("Editing mode");
-  }
-  handleSaveEdit = (user) =>{
+  };
+  handleSaveEdit = user => {
     this.props.updateUser(user);
-    console.log(this.props.updateUser(user))
-  }
+    console.log(this.props.updateUser(user));
+  };
 
   render() {
     const { users } = this.props.user;
-    const renderUser = users.map(user=>(
+    const renderUser = users.map(user => (
       <ul className="user-lists" key={user.name}>
         <li>{user.name}</li>
         <li>{user.email}</li>
         <li>{user.country}</li>
-        <li><button onClick={(name)=>this.handleDelete(user.name)}>Delete</button></li>
-        <li><button onClick={(name)=>this.handleEdit(user)}>Edit</button></li>
+        <li>
+          <button onClick={name => this.handleDelete(user.name)}>Delete</button>
+        </li>
+        <li>
+          <button onClick={name => this.handleEdit(user)}>Edit</button>
+        </li>
       </ul>
-    )); 
+    ));
     return (
       <div>
-        {this.state.isEditing ?
-        <EditUser initialValues = 
-        {{
-          name:this.state.editUserData.name,
-          email:this.state.editUserData.email,
-          country:this.state.editUserData.country,
-        }}
-        onSubmit={this.handleSaveEdit}
-        /> : 
-          <AddUser onSubmit={this.handleSubmit}/>}
+        <h1 align="center">React Redux Crud Application</h1>
+        <div className="app-form">
+          {this.state.isEditing ? (
+            <EditUser
+              initialValues={{
+                name: this.state.editUserData.name,
+                email: this.state.editUserData.email,
+                country: this.state.editUserData.country
+              }}
+              onSubmit={this.handleSaveEdit}
+            />
+          ) : (
+            <AddUser onSubmit={this.handleSubmit} />
+          )}
+        </div>
+        <div className="app-list">
           <h1>This is userlist</h1>
-            {renderUser}
+          {renderUser}
+        </div>
       </div>
     );
   }
 }
 
 UserList.propTypes = {
-  getUsers:PropTypes.func.isRequired,
-  user:PropTypes.object.isRequired
+  getUsers: PropTypes.func.isRequired,
+  user: PropTypes.object.isRequired
 };
-const mapStateToProps = (state) =>({
-  user:state.user
+const mapStateToProps = state => ({
+  user: state.user
 });
-export default connect(mapStateToProps, { getUsers, getDelete, addUsers, getEdit,updateUser })(UserList);
+export default connect(
+  mapStateToProps,
+  { getUsers, getDelete, addUsers, getEdit, updateUser }
+)(UserList);
